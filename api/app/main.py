0ff633ea290308daa27f9302612dea87c37f6b1e
Venca Srubar <vaclavcarlosrubar@gmail.cz>
2026-06-24 08:46:20 +0200

    FIxed main page + add basic styling + fixed path to static + tested

--- api/app/main.py
+++ api/app/main.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from api.app.routers import folders, notes
@@ -8,9 +10,13 @@ app = FastAPI()
 app.include_router(folders.router)
 app.include_router(notes.router)
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-templates = Jinja2Templates(directory="templates")
+static_dir = os.path.join(BASE_DIR, "static")
+
+app.mount("/static", StaticFiles(directory=static_dir), name="static")
+
+templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
 
 @app.get("/", response_model=None)
 def main(request: Request):
