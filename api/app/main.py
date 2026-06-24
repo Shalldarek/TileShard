b8e8dcb07ee8ecbf09a54b1a9040b2334165654e
Venca Srubar <vaclavcarlosrubar@gmail.cz>
2026-06-24 08:33:16 +0200

    Add Jinja + first main page + tested

--- api/app/main.py
+++ api/app/main.py
@@ -1,11 +1,19 @@
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from api.app.routers import folders, notes
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 app.include_router(folders.router)
 app.include_router(notes.router)
 
-@app.get("/")
-def main():
-    return {"message": "Welcome to TileShard API!"}
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_model=None)
+def main(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="mainpage.html"
+    )
